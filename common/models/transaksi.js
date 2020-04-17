e57a561c5877bf9656bef6951633621ff3d369f8
yordan <rian17ti@mahasiswa.pcr.ac.id>
2020-04-17 17:26:26 +0700

    edit
    edit

--- common/models/transaksi.js
+++ common/models/transaksi.js
@@ -2,4 +2,23 @@
 
 module.exports = function(Transaksi) {
 
+    Transaksi.discount = function (num1,num2,cb) {
+        // Transaksi.findById(tID, function (err, instance) {
+            // var diskon = instance.Total_Harga * instance.Diskon / 100
+            // var result = instance.Total_Harga - diskon;
+            var diskon = num1 * num2 / 100
+            var result = num1 - diskon
+            cb(null, result);
+        // });
+    }
+
+    Transaksi.remoteMethod('discount',
+    {
+        http: {verb:'post', path:'/discount'},
+        // accepts: {arg: 'id', type: 'string', required: true, http: { source: 'query' }},
+        accepts: [{arg:'Total Harga', type:'number'},{arg:'Diskon', type:'number'}],
+        returns: {arg:'Total Harga Akhir', type: 'number'},
+        description: "Custom discount endpoint"
+    }
+    );
 };
